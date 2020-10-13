784571995fd3e8c85f387e6b9e763105eab1945b
LYCGGG <2741578755@qq.com>
2020-10-13 19:27:12 +0800

    tabLayout测试成功

---
+++ tablayouttest/src/main/java/com/lyc/tablayouttest/MyTabLayout.java
@@ -0,0 +1,29 @@
+package com.lyc.tablayouttest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.tabs.TabLayout;
+
+/**
+ * @author ：LYC
+ * @date ：Created in 2020/10/9
+ * @version:
+ * @description：
+ */
+public class MyTabLayout extends TabLayout {
+    public MyTabLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public MyTabLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTabLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ tablayouttest/src/main/java/com/lyc/tablayouttest/MyViewPager.java
@@ -0,0 +1,24 @@
+package com.lyc.tablayouttest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+/**
+ * @author ：LYC
+ * @date ：Created in 2020/10/9
+ * @version:
+ * @description：
+ */
+public class MyViewPager extends ViewPager {
+    public MyViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public MyViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
